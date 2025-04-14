384125641ce00fb99a8c044ea094df0f1bafdf6a
Alston Liu <alstonliu730@gmail.com>
2025-04-14 12:01:22 -0400

    Attempt to fix extraction of points.

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -14,19 +14,35 @@ size_t import_data(char* input_file, std::vector<DataPoint>& points) {
 
     std::string line;
     while (std::getline(file, line)) {
-        std::istringstream iss(line);
+        // Split the line by commas
+        std::string delimiter = ",";
+        size_t pos = 0;
+
+        // create a vector to hold the values
         std::vector<double> values;
         double value;
-        while (iss >> value) {
+
+        // parse the line
+        while ((pos = line.find(delimiter)) != std::string::npos) {
+            std::string token = line.substr(0, pos);
+            value = std::stod(token);
+            values.push_back(value);
+            line.erase(0, pos + delimiter.length());
+        }
+
+        // add the last value
+        if (!line.empty()) {
+            value = std::stod(line);
             values.push_back(value);
         }
 
+        // create a DataPoint object and add it to the points vector
         if (!values.empty()) {
             double* data = new double[values.size()];
             std::copy(values.begin(), values.end(), data);
-	    DataPoint pt(data, values.size());
-	    points.push_back(pt);
-	    values.clear();
+            DataPoint pt(data, values.size());
+            points.push_back(pt);
+            delete[] data; // Free the allocated memory for data
         }
     }
 
